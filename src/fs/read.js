4d9f998148c678d02555bbee740a5c834649f41a
KelstonePlanet <zbkkzk@gmail.com>
2024-01-29 21:33:39 +0100

    feat: complete read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
+import fsPromises from "node:fs/promises";
+import path from "node:path";
+import url from "node:url";
+
+const filename = url.fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+const filepath = path.join(dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+     try {
+        await fsPromises.access(filepath);
+        const contents = await fsPromises.readFile(filepath, "utf-8");
+        console.log(contents);
+     } catch (error) {
+        if (error.code === "ENOENT") {
+            throw new Error("FS operation failed");
+        } else {
+            throw error;
+        };
+     };
 };
 
 await read();
