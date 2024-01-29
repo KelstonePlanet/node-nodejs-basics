7a30deeb04b0ba6ddd5d9a19278567b399072110
KelstonePlanet <zbkkzk@gmail.com>
2024-01-29 18:19:00 +0100

    init: start new task; feat: add create task solution;

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from "fs/promises";
 const create = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        await fs.access("./src/fs/files/fresh.txt");
+        throw new Error("FS operation failed");
+    } catch (error) {
+        if (error.code === "ENOENT") {
+            try {
+                await fs.writeFile("./src/fs/files/fresh.txt", "I am young");
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            throw error;
+        }
+    }
 };
 
 await create();
